c7763a823f4267cd8bc7cb231c8bf1756d1b9672
Horia Coman <horia141@gmail.com>
2018-01-21 08:29:15 +0200

    Add support for messages from the frontend services

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,17 @@ export {
 } from './language'
 
 export {
+    Message,
+    MessageWith0Arg,
+    MessageWith1Arg,
+    MessageWith2Arg,
+    MessageWith3Arg,
+    MessageWith4Arg,
+    MessageWith5Arg,
+    MessageWith6Arg,
+    MessageWith7Arg
+} from './messages'
+
+export {
     slugify
 } from './slugify'
---
+++ src/messages.ts
@@ -0,0 +1,43 @@
+/**
+ * Defines types for translation system messages. A message is a map from a ISO 639-1 language code
+ * to the string representation of the message in that language. The message can be parametrized
+ * by up to 7 arguments.
+ */
+
+/** A message where strings are not parametrized. */
+export type MessageWith0Arg = { [key: string]: string };
+
+/** A message where strings are parametrized with 1 argument. */
+export type MessageWith1Arg = { [key: string]: (arg1: any) => string }
+
+/** A message where strings are parametrized with 2 arguments. */
+export type MessageWith2Arg = { [key: string]: (arg1: any, arg2: any) => string }
+
+/** A message where strings are parametrized with 3 arguments. */
+export type MessageWith3Arg = { [key: string]: (arg1: any, arg2: any, arg3: any) => string }
+
+/** A message where strings are parametrized with 4 arguments. */
+export type MessageWith4Arg = { [key: string]: (arg1: any, arg2: any, arg3: any, arg4: any) => string }
+
+/** A message where strings are parametrized with 5 arguments. */
+export type MessageWith5Arg = { [key: string]: (arg1: any, arg2: any, arg3: any, arg4: any, arg5: any) => string }
+
+/** A message where strings are parametrized with 6 arguments. */
+export type MessageWith6Arg = { [key: string]: (arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any) => string }
+
+/** A message where strings are parametrized with 7 arguments. */
+export type MessageWith7Arg = { [key: string]: (arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any) => string }
+
+/** The full message type. A message is a map from a ISO 639-1 language code
+ * to the string representation of the message in that language. The message can be parametrized
+ * by up to 7 arguments.
+ */
+export type Message =
+    MessageWith0Arg |
+    MessageWith1Arg |
+    MessageWith2Arg |
+    MessageWith3Arg |
+    MessageWith4Arg |
+    MessageWith5Arg |
+    MessageWith6Arg |
+    MessageWith7Arg;
